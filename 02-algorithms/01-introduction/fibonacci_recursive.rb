e057add4a4c49cd68ccd74beaf0c9981cd7c4eaa
Lisa Min <lisajmin@gmail.com>
2018-01-18 09:53:51 -0800

    Checkpoint-9: Algorithms: Search Solutions

---
+++ 02-algorithms/01-introduction/fibonacci_recursive.rb
@@ -0,0 +1,20 @@
+def fib_recursive(n)
+   if (n == 0)
+     return 0
+   elsif (n == 1)
+     return 1
+   else
+     return fib(n-1) + fib(n-2)
+   end
+ end
+
+ puts fib(0)
+ puts fib(1)
+ puts fib(2)
+ puts fib(3)
+ puts fib(4)
+ puts fib(5)
+ puts fib(6)
+ puts fib(7)
+ puts fib(8)
+ puts fib(9)
